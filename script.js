65d487e92dff29acefaeda23a2aa26c0d6c6beea
IpGa13 <darealipga13@gmail.com>
2022-03-19 23:59:45 +0100

    u1

--- script.js
+++ script.js
@@ -1,10 +1,14 @@
 function checkLogin() {
     if(document.getElementById("name").value == "root" && document.getElementById("pw").value == "toor"){
         
-        window.location.href = "webapp.html";
+        window.location.href = "devapp.html";
     }
 
     else if(document.getElementById("name").value == "jhnor" && document.getElementById("pw").value == "Irrtum33") {
+        window.location.href = "premiumapp.html";
+    }
+
+    else if(document.getElementById("name").value == "guest" && document.getElementById("pw").value == "") {
         window.location.href = "webapp.html";
     }
 
