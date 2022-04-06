d25f82bca1fb50aac4c70f25ccef65c7f8c2f1a3
IpGa13 <83000938+IpGa13@users.noreply.github.com>
2022-04-06 23:44:37 +0200

    changes

--- script.js
+++ script.js
@@ -1,10 +1,16 @@
+//Yep. all login data is stored in plaintext and hardcoded. im not paying for an actual server to host this joke.
+
 function checkLogin() {
     if(document.getElementById("name").value == "root" && document.getElementById("pw").value == "toor"){
         
         window.location.href = "devapp.html";
     }
 
-    else if(document.getElementById("name").value == "jhnor" && document.getElementById("pw").value == "Irrtum33") {
+    else if(document.getElementById("name").value == "jhnor" && document.getElementById("pw").value == "earthround") {
+        window.location.href = "premiumapp.html";
+    }
+
+    else if(document.getElementById("name").value == "soren" && document.getElementById("pw").value == "LeanIsTheBestDrug") {
         window.location.href = "premiumapp.html";
     }
 
